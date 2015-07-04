55b635dd82bfdbfc55acb22c290e42660f7f8001
unknown <ajith@Ajith-PC.corp.empressem.net>
2015-07-04 17:57:41 +0530

    adding new files

---
+++ OneConnect/OneConnect/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OneConnect.Controllers
+{
+    public class UserController : Controller
+    {
+        HomeController home = new HomeController();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+
+            ViewBag.isRegistrationCompletedSuccessfully = false;
+            return View();
+        }
+        public ActionResult Dashboard()
+        {
+            if(home.IsAuthenticated())
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index","Home");
+            }
+        }
+    }
+}
