2fdf2aef528b284cd92039967b03d523f374f33e
ajith chandran <ajithchandran1990@gmail.com>
2015-08-19 22:26:48 +0530

    example commit

--- OneConnect/OneConnect/Controllers/Api/GroupController.cs
+++ OneConnect/OneConnect/Controllers/Api/GroupController.cs
@@ -12,26 +12,26 @@ namespace OneConnect.Controllers.Api
     public class GroupController : ApiController
     {
         OneKonnectEntities DBEntities = new OneKonnectEntities();
-        //POST api/Group/GetGroups
+        //POST api/Group/GetGroupsakwdksdjfhjksdhfkjdhsjk
         [HttpGet]
         [Authorize]
         [Route("GetGroups")]
         public IEnumerable<GroupDetails> GetGroups()
         {
             IEnumerable<GroupDetails> list = null;
-            //try
-            //{
-            //    AccountController acc = new AccountController();
-            //    string userId = acc.GetUserIdByName(User.Identity.Name);
+            try
+            {
+                AccountController acc = new AccountController();
+                string userId = acc.GetUserIdByName(User.Identity.Name);
 
-            //    list = (from g in DBEntities.Groups
-            //            where g.GroupOwner == userId 
-            //            select new { r = g }).Select(t => new GroupDetails { groupId = t.r.Id, groupName = t.r.Name, description = t.r.Description,isActive=(bool)t.r.IsActive}).ToList();
+                list = (from g in DBEntities.Groups
+                        where g.GroupOwner == userId
+                        select new { r = g }).Select(t => new GroupDetails { groupId = t.r.Id, groupName = t.r.Name, description = t.r.Description, isActive = (bool)t.r.IsActive }).ToList();
 
-            //}
-            //catch (Exception e)
-            //{
-            //}
+            }
+            catch (Exception e)
+            {
+            }
             return list;
         }
 
